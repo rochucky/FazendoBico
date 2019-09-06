0509ddd1fdef26cc26892b6a69028d337cd2ca81
rochucky <alves.rodrigo31@outlook.com>
2019-09-06 02:56:22 -0300

    Selecionar o tipo de usuário

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -12,58 +12,83 @@ export default class SignUp extends React.Component {
       name: '',
       email: '',
       pass: '',
-      passConfirm: ''
+      passConfirm: '',
+      type: ''
     }
     this.users = firebase.firestore().collection('users');
   }
 
 
   render(){
-    return(
-      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-        <Image style={styles.logo} source={require('../assets/images/tags.png')} />
-        <Text style={styles.text} category='h4'>Cadastro</Text>
-        <Input 
-          style={styles.input}
-          placeholder='Nome Completo' 
-          onChangeText={(text) => this.setState({name: text})} 
-          value={this.state.name}
-        />
-        <Input 
-          style={styles.input}
-          placeholder='Email' 
-          onChangeText={(text) => this.setState({email: text})} 
-          value={this.state.email}
-        />
-        <Input 
-          style={styles.input}
-          placeholder='Senha' 
-          onChangeText={(text) => this.setState({pass: text})} 
-          value={this.state.pass}
-          secureTextEntry={true}
-        />
-        <Input 
-          style={styles.input}
-          placeholder='Confirme a Senha' 
-          onChangeText={(text) => this.setState({passConfirm: text})} 
-          value={this.state.passConfirm}
-          secureTextEntry={true}
-        />
-        <Button 
-          style={styles.button}
-          onPress={this.SignUp.bind(this)}
-          title='Login' 
-        >Cadastrar</Button>
-        <Text
-          style={styles.links}
-          category='h5'
-          status='info'
-          onPress={() => {
-            this.props.navigation.goBack()
-          }}
-        >Voltar</Text>
-      </KeyboardAvoidingView>
-    )
+    if(this.state.type == ''){
+      return(
+        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+          <Image style={styles.logo} source={require('../assets/images/tags.png')} />
+          <Text style={styles.text} category='h4'>Cadastro</Text>
+          <Button 
+            style={styles.button}
+            onPress={() => {
+              this.setState({type: 'cliente'})
+            }}
+          >Sou um Cliente</Button>
+          <Button 
+            style={styles.button}
+            onPress={() => {
+              this.setState({type: 'freelancer'})
+            }}
+          >Sou um Freelancer</Button>
+        </KeyboardAvoidingView>
+
+      )
+    }
+    else{
+
+      return(
+        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+          <Image style={styles.logo} source={require('../assets/images/tags.png')} />
+          <Text style={styles.text} category='h4'>Cadastro</Text>
+          <Input 
+            style={styles.input}
+            placeholder='Nome Completo' 
+            onChangeText={(text) => this.setState({name: text})} 
+            value={this.state.name}
+          />
+          <Input 
+            style={styles.input}
+            placeholder='Email' 
+            onChangeText={(text) => this.setState({email: text})} 
+            value={this.state.email}
+          />
+          <Input 
+            style={styles.input}
+            placeholder='Senha' 
+            onChangeText={(text) => this.setState({pass: text})} 
+            value={this.state.pass}
+            secureTextEntry={true}
+          />
+          <Input 
+            style={styles.input}
+            placeholder='Confirme a Senha' 
+            onChangeText={(text) => this.setState({passConfirm: text})} 
+            value={this.state.passConfirm}
+            secureTextEntry={true}
+          />
+          <Button 
+            style={styles.button}
+            onPress={this.SignUp.bind(this)}
+            title='Login' 
+          >Cadastrar</Button>
+          <Text
+            style={styles.links}
+            category='h5'
+            status='info'
+            onPress={() => {
+              this.props.navigation.goBack()
+            }}
+          >Voltar</Text>
+        </KeyboardAvoidingView>
+      )
+    }
   }
   SignUp = () => {
     if(this.state.email == ''){
@@ -74,7 +99,8 @@ export default class SignUp extends React.Component {
       firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.pass).then((response) => {
         this.users.add({
           name: this.state.name,
-          email: this.state.email
+          email: this.state.email,
+          type: this.state.type
         }).then((result) => {
           alert('Usuário cadastrado com sucesso');
           this.props.navigation.goBack();
